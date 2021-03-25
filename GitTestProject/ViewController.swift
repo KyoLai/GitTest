144fd4f8dda37dfe4df88783c8248debfc0d39c9
Görkem KyoLai <gorkembgt06@icloud.com>
2021-03-25 10:47:09 +0300

    first commit i've remove the print("asdfasdf")

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Gorkem on 25.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+        print("asdfasdf")
+        
+    }
+
+
+}
+
