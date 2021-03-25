62a5a5d36433ed78b17e6975ce416e899e0379e3
Görkem KyoLai <gorkembgt06@icloud.com>
2021-03-25 10:48:45 +0300

    second commit

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("git")
-        print("asdfasdf")
+        print("git2")
         
     }
 
